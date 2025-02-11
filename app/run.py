73d850c27b7d3cf2f60cb0aea1326654ccac4c61
Chetan Rathod <chetanrathodins03@gamil.com>
2025-02-12 02:16:58 +0530

    fourt commit run.py show the warnings

--- app/run.py
+++ app/run.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask import Flask, render_template, jsonify
-import feedparser
+import feedparser # type: ignore
 from app.routes.prediction_routes import prediction_bp  # Ensure correct import path
 
 app = create_app()
